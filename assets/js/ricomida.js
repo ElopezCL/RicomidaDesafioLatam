e1c3dfef14cc99a00d17ba2803ff47a774ffc3f7
ElopezCL <em.lopezcortes@gmail.com>
2024-03-10 17:49:09 -0300

    Haciendo cambios

--- assets/js/ricomida.js
+++ assets/js/ricomida.js
@@ -7,6 +7,16 @@ $('#enviar_correo').click(function () {
     alert("Correo enviado correctamente")
 })
 
+
+$("h5").click(function () {
+
+    $(this).css({
+        "color": "red",
+        "font-size": "2rem"
+    })
+})
+
+
 $('.card-title').click(function(){
     $('.recetas-relacionadas p').toggle("hide");
 })
